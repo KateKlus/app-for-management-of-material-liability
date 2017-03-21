98f9e1b145a39e1d87955b0de97e9018254238c9
KateKlus <ekaterina.klus@gmail.com>
2017-03-21 21:39:02 +1000

    Добавлена модель пользователя GLPI

--- rest_service/admin.py
+++ rest_service/admin.py
@@ -3,3 +3,4 @@ from models import *
 # Register your models here.
 admin.site.register(MO)
 admin.site.register(Attribute)
+admin.site.register(GLPI_user)
--- rest_service/models.py
+++ rest_service/models.py
@@ -2,15 +2,6 @@
 from __future__ import unicode_literals
 from django.db import models
 
-#абстрактный родитель МО
-class MO_abstract(models.Model):
-    name = models.CharField("Название", max_length=45)
-    serial = models.CharField("Инвентарный номер", max_length=30)
-    contact = models.CharField("Ответственное лицо", max_length=45)
-
-    class Meta:
-       abstract = True
-
 #модель материального объекта
 class MO(models.Model):
     MO_id = models.AutoField(primary_key=True)
@@ -47,11 +38,47 @@ class Attribute(models.Model):
     def __unicode__(self):
         return self.attr_name
 
+
+#модель пользователя GLPI
+class GLPI_user(models.Model):
+    name = models.CharField("Логин", max_length=255)
+    realname = models.CharField("Фамилия", max_length=255)
+    firstname = models.CharField("Имя", max_length=255)
+    user_dn = models.TextField("Информация о пользователе")
+
+    class Meta:
+        db_table = 'glpi_users'
+        app_label = 'clientapp'
+        verbose_name = 'Ответственный специалист'
+        verbose_name_plural = 'Ответственные специалисты'
+        ordering = ['realname']
+
+    def __unicode__(self):
+        user_data = unicode(self.user_dn).split(',')[0]
+        if user_data == 'None':
+            return self.name
+        else:
+            return user_data[3:]
+
+#абстрактный родитель МО
+class MO_abstract(models.Model):
+    name = models.CharField("Название", max_length=45)
+    serial = models.CharField("Инвентарный номер", max_length=30)
+    contact = models.ForeignKey(
+        GLPI_user,
+        verbose_name="Ответственное лицо",
+    )
+
+    class Meta:
+       abstract = True
+
+
 #описание моделей БД GLPI
 class Location(models.Model):
     name = models.CharField("Аудитория", max_length=200)
     entities_id = models.IntegerField()
     locations_id = models.IntegerField()
+    
     class Meta:
         db_table = 'glpi_locations'
         app_label = 'glpi'
@@ -60,6 +87,7 @@ class Location(models.Model):
     def __unicode__(self):
         return self.name
 
+#модель компьютера
 class Computer(MO_abstract):
     locations = models.ForeignKey(
          Location,
@@ -70,6 +98,7 @@ class Computer(MO_abstract):
         db_table = 'glpi_computers'
         app_label = 'glpi'
 
+#модель монитора
 class Monitor(MO_abstract):
     locations = models.ForeignKey(
          Location,
