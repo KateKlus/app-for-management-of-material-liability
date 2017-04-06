0eb9319661f4bc9ac8d8365440f819367c9b4be7
KateKlus <ekaterina.klus@gmail.com>
2017-04-06 18:10:14 +1000

    Вынесен повторяющийся код вывода оборудования

--- rest_service/static/js/script.js
+++ rest_service/static/js/script.js
@@ -1,3 +1,20 @@
 /**
  * Created by catherine on 06.04.17.
  */
+ $(document).ready(function() {
+        $('.mo_name').click(function () {
+            $(this).parent().children('div.mo_detail').toggle('normal');
+            return false;
+        });
+
+        $('.mo_group_name').click(function(){
+            $(this).parent().children('.mo_group_result').toggle();
+            return false;
+        });
+
+         $('.parent_loc').click(function(){
+            $(this).parent().children('li.children_loc').toggle('normal');
+            return false;
+        });
+    });
+
