3800cfc9a8d987e7eebe775933beb3e9353ae8fc
KateKlus <ekaterina.klus@gmail.com>
2017-05-31 17:03:11 +1000

    Правки к релизу

---
+++ static/js/ajax.js
@@ -0,0 +1,221 @@
+function get_mo_list(location) {
+    var loc_id = location.getAttribute("name");
+
+    $.ajax({
+        url: 'get_mo_list/'+loc_id,
+        dataType: 'html',
+        success: function (data) {
+            $('#mo_list_res').html(data);
+            }
+          });
+    }
+
+function get_mo_detail(mo) {
+    var mo_id = mo.getAttribute("name");
+
+    $.ajax({
+        url: 'get_mo_detail/'+mo_id,
+        dataType: 'html',
+        success: function (data) {
+            $('#mo_detail_res').html(data);
+            }
+          });
+    }
+
+function get_comp_detail(mo) {
+    var mo_id = mo.getAttribute("name");
+
+    $.ajax({
+        url: 'get_comp_detail/'+mo_id,
+        dataType: 'html',
+        success: function (data) {
+            $('#'+mo_id+'_detail').html(data);
+            }
+          });
+    }
+
+function get_monitor_detail(mo) {
+    var mo_id = mo.getAttribute("name");
+
+    $.ajax({
+        url: 'get_monitor_detail/'+mo_id,
+        dataType: 'html',
+        success: function (data) {
+            $('#'+mo_id+'mon_detail').html(data);
+            }
+          });
+    }
+
+function update_mo(mo_id) {
+    update_mo_id = mo_id;
+    var url = '../update_mo/'+mo_id;
+    ajax_update_mo(mo_id, url);
+}
+
+function update_comp (mo_id) {
+    update_mo_id = mo_id;
+    var url = '../update_comp/'+mo_id;
+    ajax_update_mo(mo_id, url);
+}
+
+function update_monitor (mo_id) {
+    update_mo_id = mo_id;
+    var url = '../update_monitor/'+mo_id;
+    ajax_update_mo(mo_id, url);
+}
+
+function push_mo() {
+    var url = '../update_mo/'+update_mo_id;
+
+    var form = document.forms["mo_update_form"];
+    var data = {
+            location: form.elements["id_location"].value,
+            name: form.elements["id_name"].value,
+            serial: form.elements["id_serial"].value,
+            contact: form.elements["id_contact"].value,
+            mo_type: form.elements["id_mo_type"].value,
+            note: form.elements["id_note"].value
+        };
+
+    ajax_push_update_mo(data, url);
+}
+
+function push_comp() {
+    var url = '../update_comp/'+update_mo_id;
+
+    var form = document.forms["mo_update_form"];
+    var data = {
+            locations: form.elements["id_locations"].options[form.elements["id_locations"].selectedIndex].value,
+            name: form.elements["id_name"].value,
+            serial: form.elements["id_serial"].value,
+            otherserial: form.elements["id_otherserial"].value,
+            users_id_tech: form.elements["id_users_id_tech"].options[form.elements["id_users_id_tech"].selectedIndex].value
+        };
+
+    ajax_push_update_mo(data, url);
+}
+
+function push_monitor() {
+    var url = '../update_monitor/'+update_mo_id;
+
+    var form = document.forms["mo_update_form"];
+    var data = {
+            locations: form.elements["id_locations"].options[form.elements["id_locations"].selectedIndex].value,
+            name: form.elements["id_name"].value,
+            serial: form.elements["id_serial"].value,
+            users_id_tech: form.elements["id_users_id_tech"].options[form.elements["id_users_id_tech"].selectedIndex].value
+        };
+
+    ajax_push_update_mo(data, url);
+}
+
+//ajax post запрос на обновление оборудования
+function ajax_push_update_mo(data, url){
+
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings)
+        {
+            function getCookie(name)
+            {
+                var cookieValue = null;
+                if (document.cookie && document.cookie != '')
+                {
+                    var cookies = document.cookie.split(';');
+                    for (var i = 0; i < cookies.length; i++)
+                    {
+                        var cookie = jQuery.trim(cookies[i]);
+                        // Does this cookie string begin with the name we want?
+                        if (cookie.substring(0, name.length + 1) == (name + '='))
+                        {
+                            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                            break;
+                        }
+                    }
+                }
+                return cookieValue;
+            }
+
+            if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url)))
+            {
+                // Only send the token to relative URLs i.e. locally.
+                xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+            }
+        }
+    });
+
+    $.ajax({
+        method: "POST",
+        url: url,
+        data: data,
+        beforeSend: function(xhr, settings) {
+            $.ajaxSettings.beforeSend(xhr, settings);
+        },
+
+        success : function(json) {
+            alert("Данные успешно обновлены");
+        },
+        
+        error : function(xhr,errmsg,err) {
+            alert("Ошибка");
+        }
+
+        });
+}
+
+//ajax get запрос на обновление оборудования
+function ajax_update_mo(mo_id, url) {
+    $.ajax({
+        url: url,
+        data: mo_id,
+        dataType: 'html',
+        success: function (data) {
+            $('.edit_mo').html(data);
+            }
+          });
+
+    var modal = document.getElementById('myModal');
+    var btn = document.getElementById("myBtn");
+    var span = document.getElementsByClassName("close")[0];
+
+    modal.style.display = "block";
+
+    btn.onclick = function() {
+        modal.style.display = "block";
+    };
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    };
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
+function comp_detail_info(comp) {
+
+    $.ajax({
+        url: '../get_comp_detail_info/'+ comp,
+        dataType: 'html',
+        success: function (data) {
+            $('.comp_detail_info').html(data);
+            }
+          });
+
+        var modal = document.getElementById('myModal');
+        var span = document.getElementsByClassName("close")[0];
+
+        modal.style.display = "block";
+
+        span.onclick = function() {
+            modal.style.display = "none";
+        };
+
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+    }
---
+++ static/js/script.js
@@ -0,0 +1,206 @@
+/**
+ * Created by catherine on 06.04.17.
+ */
+
+var update_mo_id;
+var mo_list = [];
+
+$(document).ready(function() {
+    $('.mo_name').click(function () {
+        $(this).parent().children('div.mo_detail').toggle('normal');
+        return false;
+    });
+
+    $('.mo_group_name').click(function(){
+        $(this).parent().children('.mo_group_result').toggle();
+        return false;
+    });
+
+    $('.parent_loc').click(function(){
+        $(this).parent().children('li.children_loc').toggle('normal');
+        return false;
+    });
+
+    $('.root_ent').click(function(){
+        $(this).parent().children('ul.parent_ent').toggle('normal');
+        return false;
+    });
+
+    $('.parent_ent').click(function(){
+        $(this).children('.children_ent').toggle('normal');
+        return false;
+    });
+
+    $('.first_letter').click(function () {
+        $(this).parent().children('.types').toggle('normal');
+        return false;
+    });
+
+     $('.types').click(function () {
+        $(this).next('.types_result').toggle('normal');
+        return false;
+    });
+
+     $('.mo_by_loc').click(function () {
+        $(this).next('.types_result').toggle('normal');
+        return false;
+    });
+
+    $('.types_mo_group_result').hover(
+        function(){
+             $(this).children('label').children('.checkbox-custom').css('visibility', 'visible');
+        },
+        function(){
+            if ($(this).children('label').children('input').prop("checked")) {
+                $(this).children('label').children('.checkbox-custom').css('visibility', 'visible');
+            }
+            else{
+                 $(this).children('label').children('.checkbox-custom').css('visibility', 'hidden');
+            }
+        });
+
+    var checkboxes = document.getElementsByTagName('input');
+
+    for (var n=0; n<checkboxes.length; n++)  {
+        if (checkboxes[n].type == 'checkbox')   {
+            checkboxes[n].checked = false;
+        }
+    }
+
+
+    var i = 0;
+    $(":checkbox").change(function(){
+        if(this.checked){
+            mo_list[i] = this.name;
+            i++;
+            if (i == 1){
+                $('.bottom_menu').animate({'margin-bottom': 0}, 300)
+            }
+        }else{
+            var val = this.name;
+            var index = mo_list.indexOf(val);
+            mo_list.splice(index, 1);
+            i--;
+            if (i == 0){
+                $('.bottom_menu').animate({'margin-bottom': -80}, 300)
+            }
+        }
+
+    });
+
+});
+
+function show_modal(){
+    var modal = document.getElementById('myModal');
+        var span = document.getElementsByClassName("close")[0];
+
+        modal.style.display = "block";
+
+        span.onclick = function() {
+            modal.style.display = "none";
+        };
+
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+}
+
+function set_alphabet() {
+    $('#alph').css('color',  'black');
+    $('#loc').css('color',  'cadetblue');
+    $('.location_lists').css('display', 'none');
+    $('.alphabet_lists').css('display', 'block');
+}
+
+function set_locations() {
+    $('#loc').css('color',  'black');
+    $('#alph').css('color',  'cadetblue');
+    $('.alphabet_lists').css('display', 'none');
+    $('.location_lists').css('display', 'block');
+}
+
+//ajax post запрос на обновление оборудования
+function ajax_push_update_mo(data, url) {
+
+    $.ajaxSetup({
+        beforeSend: function (xhr, settings) {
+            function getCookie(name) {
+                var cookieValue = null;
+                if (document.cookie && document.cookie != '') {
+                    var cookies = document.cookie.split(';');
+                    for (var i = 0; i < cookies.length; i++) {
+                        var cookie = jQuery.trim(cookies[i]);
+                        // Does this cookie string begin with the name we want?
+                        if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                            break;
+                        }
+                    }
+                }
+                return cookieValue;
+            }
+
+            if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+                // Only send the token to relative URLs i.e. locally.
+                xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+            }
+        }
+    });
+
+    $.ajax({
+        method: "POST",
+        url: url,
+        data: data,
+        beforeSend: function (xhr, settings) {
+            $.ajaxSettings.beforeSend(xhr, settings);
+        },
+
+        success: function (json) {
+            alert("Данные успешно обновлены");
+        },
+
+        error: function (xhr, errmsg, err) {
+            alert("Ошибка");
+        }
+
+    });
+
+}
+
+//ajax get запрос на обновление оборудования
+function ajax_transfer_mo(loc) {
+
+    var data = {
+            loc: loc,
+            mo_list: JSON.stringify(mo_list)
+        };
+
+    $.ajax({
+        url: './mo_transfer/json/',
+        data: data,
+        dataType: 'json',
+         success : function(json) {
+             $('.edit_mo').html('<p>Изменения успешно внесены </p>');
+        },
+
+        error : function(xhr,errmsg,err) {
+            $('.edit_mo').html('<p>Аудитория введена не верно. Изменения не были внесены</p>');
+        }
+    });
+    show_modal();
+}
+
+
+function mo_transfer() {
+    var loc = document.getElementById("location_name").value;
+    if(loc == ''){
+        $('.edit_mo').html('<p>Укажите аудиторию!</p>');
+        show_modal();
+
+    }
+    else{
+        ajax_transfer_mo(loc);
+    }
+}
